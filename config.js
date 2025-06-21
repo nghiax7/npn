5e3ccca48882a682e96864a53a378d26ac7bd42b
nghiax7 <nghiax7tcfc37@gmail.com>
2025-06-21 12:50:57 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Anh với cái trống thì em để ý cái nào hơn nhỉ :v 😙',
+    desc: 'Liệu eim có đồng ý ',
+    btnYes: 'Vaanggg, eim đồng ý <33',
+    btnNo: 'Không, Anh trai à :3',
+   question: ' Kiểm tra bài cũ nhé!! Đồ thị hàm số y = x^3 - 3x có mấy điểm cực trị?',
+    btnReply: 'Trả lờii ',
+    reply: 'Yêu thì yêu mà không yêu thì thôiii haha',
+    mess: 'Vẫn thông minh đấy',
+    messDesc: 'Tối nay anh sẽ ru em ngủ ngonn nhé.',
+    btnAccept: 'Nhấp vào đây đi bé',
+    messLink: '/tang-crush-main/galaxy-viewer.html' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
